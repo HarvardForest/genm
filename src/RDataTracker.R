07cd262d6819c4393de05ffa7b3e973071852e64
anncalderon <acalderon@ucmerced.edu>
2016-06-16 16:35:59 -0400

    Updated gENM

---
+++ src/RDataTracker.R
@@ -0,0 +1,18 @@
+# setwd("/Users/annacalderon/Desktop/gENM/src/")
+# ddg.run("RDataTracker.R")
+# install.packages("RDataTracker")
+#################### 4.1 A Simple Example ##############################
+
+
+
+
+
+#######################USING STRUCTURE#################################
+x <- c("green","red","blue", "Yellow", "purple", "yellowgreen", "pink", "aliceblue", "turquoise", "magenta")
+structure(x)
+
+y<-c(1:10)
+structure(y)
+y
+
+#tell barbara to fix the RDATATRACKER installation problem
