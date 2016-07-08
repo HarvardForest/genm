eebb06fa53d312e2631ea15d74a533383277c6de
anncalderon <acalderon@ucmerced.edu>
2016-07-08 10:20:25 -0400

    Changed bg to random

--- src/SDM_template.R
+++ src/SDM_template.R
@@ -135,7 +135,7 @@ me
 
 plot(pred_me, 1, cex=0.5, legend=T, mar=par("mar"), xaxt="n", yaxt="n", main="Predicted Species Distribution")
 map("state", xlim=c(leftlon,rightlon), ylim=c(lowerlat,upperlat), fill=F, col="black", add=T)
-points(bg,col="snow",pch=1,cex=0.2)
+points(random,col="snow",pch=1,cex=0.2)
 points(Gspecies$lon, Gspecies$lat, col="darkgreen", pch=20, cex=0.5)
 
 
