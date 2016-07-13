5e0f0ccad8b99764e44d071d7acfe1a9a9d245b4
mklau <matthewklau@fas.harvard.edu>
2016-07-13 17:17:12 -0400

    matt fixing genetic clustering

--- src/Clustering.R
+++ src/Clustering.R
@@ -14,14 +14,11 @@ library(fossil)
 library(igraph)
 
 ## Step 2. Setting Seed/N
-
-
-
 N <- ''
 
-###########################################################################
 
-altDiff 
+
+###########################################################################
 hd <- transition(neClim$bio1, altDiff, 8, symm=FALSE)
 slope <- geoCorrection(hd)
 adj <- adjacent(neClim$bio1, cells=1:ncell(neClim$bio1), pairs=TRUE, directions=8)
@@ -31,30 +28,22 @@ Conductance <- geoCorrection(speed)
 
 cd <- costDistance(Conductance, gspecies)
 scd <- symSum(cd)
-m(scd)
 if (N == ''){N <- 1}
 Fst <- 0.20*(1/(1+4*N*m(scd))) #see Conner & Hartle pg. 84
-
-
+diag(Fst) <- 0
 
 ## returns a minimally connected graph
-Fst.mst <- Fst
-Fst.dm <- dino.mst(1-Fst)
-Fst.mst[Fst.dm == 0] <- 0 
-Fst.mstP <- Fst.mst
-Fst.mstP[Fst.mstP != 0] <- (1-Fst.mst[Fst.mst != 0])
+Fst.g <- 1-Fst
+diag(Fst.g) <- 0
+Fst.mg <- dino.mst(Fst.g)
+Fst.ig <- graph.adjacency(Fst.mg,weighted=TRUE,mode='undirected')
 
-Fst.ig <- graph.adjacency(Fst,weighted=TRUE,mode='undirected')
-Fstm.ig <- graph.adjacency(Fst.mst,weighted=TRUE,mode='undirected')
-Fst.igP <- graph.adjacency((1-Fst),weighted=TRUE,mode='undirected')
-Fstm.igP <- graph.adjacency(Fst.mstP,weighted=TRUE,mode='undirected')
+Fst.ig <- graph.adjacency(Fst.g,weighted=TRUE,mode='undirected')
 
-fastgreedy.community(Fst.igP)
-fastgreedy.community(Fstm.ig)
+plot(Fst.ig)
+fg.mP <- fastgreedy.community(Fst.ig)
+
 fg.mP <- fastgreedy.community(Fstm.igP)
-
-
-
 
 gclust <- fg.mP$membership
 names(gclust) <- rownames(Fst.mst)
