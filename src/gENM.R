d75a3a14f9494687a0cda09dc56f498f88602477
anncalderon <acalderon@ucmerced.edu>
2016-07-28 16:19:05 -0400

    curate scripts to gENM.R

---
+++ src/gENM.R
@@ -0,0 +1,38 @@
+#Anna M Calderon
+#Matthew K Lau
+#Harvard Forest
+#gENM-Data Set Up
+#Part 0
+#28 July 2016
+
+#Anna M Calderon
+#Matthew K Lau
+#Harvard Forest
+#gENM-Data Set Up
+#Part 0
+#8 July 2016
+
+#SET WORKING DIRECTORY
+wd <- '/Users/annacalderon/Desktop/gENM/src'
+setwd(wd)
+
+#SOURCE FUCNTIONS
+source("helpers.R")
+
+#IMPORTING DATA
+neClim <- stack("../data/neClim.grd")
+mintemp.2006  <- raster("../data/01_01_2006.tiff")
+mintemp.2050 <- raster("../data/01_01_2050.tiff")
+mintemp.2099 <- raster("../data/01_01_2099.tiff")
+
+gsp <-read.csv("../data/RICTMEdukesnantucket.csv")
+if (is.matrix(gsp) == FALSE){gsp <- data.matrix(gsp)}
+
+#CLUSTERS
+
+clust <- gClust(x=gsp, vp=mintemp.2006)
+out <- gENM(x=gsp, clust=clust, p=mintemp.2006) 
+gAnalysis(x=out)
+
+#HISTOGRAMS and GRAPHS
+
