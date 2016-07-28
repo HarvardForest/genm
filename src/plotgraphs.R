23a7d71472645588b8cae120bba97c77582a3ce3
anncalderon <acalderon@ucmerced.edu>
2016-07-28 11:04:58 -0400

    add plotgraphs.R

--- src/plotgraphs.R
+++ src/plotgraphs.R
@@ -15,6 +15,43 @@ plot(out$`2`[["pred"]], axes=F, zlim=c(0, 1), col=yellow_blue,legend=T, box=F)
 # points(groups$`2`, col="yellow", cex=1.3,pch =19)
 # points(groups$`3`, col="turquoise1", cex=1.3,pch =19)
 
+## Step 6. Making Histograms for each cluster
+
+
+gsp_bc <-  extract(mintemp.2006, gsp) 
+mt.c <- split(gsp_bc, clust)
+par(mfrow=c(1,3))
+#lapply(mt.c, hist)
+hrange <- c(265:280)
+
+pdf('~/Desktop/gENM/results/clust_hist.pdf', bg="black")
+par(mfrow=c(3,1))
+
+hist(mt.c$`1`,axes=F, col="mediumpurple1", xlim=c(265,280))
+axis(1, col = 'white', col.axis = 'white', col.ticks = 'white',cex.axis = 1.5, font = 1)
+axis(2, col = 'white', col.axis = 'white', col.ticks = 'white', cex.axis = 1.5, font =1)
+
+hist(mt.c$`2`, axes= F, col="yellow", xlim=c(265,280), ylim=c(0, 400))
+axis(1, col = 'white', col.axis = 'white', col.ticks = 'white',cex.axis = 1.5, font = 1)
+axis(2, col = 'white', col.axis = 'white', col.ticks = 'white', cex.axis = 1.5, font =1)
+
+hist(mt.c$`3`, col="turquoise1",axes= F,xlim=c(265,280))
+axis(1, col = 'white', col.axis = 'white', col.ticks = 'white',cex.axis = 1.5, font = 1)
+axis(2, col = 'white', col.axis = 'white', col.ticks = 'white', cex.axis = 1.5, font=1)
+
+
+hist(gsp_bc,col="violetred1", axes=F, xlim=c(265,280), ylim=c(0,400))
+
+axis(1, col = 'white', col.axis = 'white', col.ticks = 'white',cex.axis = 1.5, font = 1)
+axis(2, col = 'white', col.axis = 'white', col.ticks = 'white', cex.axis = 1.5, font=1)
+
+
+dev.off()
+system('open ../results/clust_hist.pdf')
+
+
+
+
 
 dev.off()
 
--- src/test.run.R
+++ src/test.run.R
@@ -14,7 +14,7 @@ source("helpers.R")
 
 
 ## Step 2. Importing Climate Variables for NE
-#neClim <- stack("../data/neClim.grd")
+neClim <- stack("../data/neClim.grd")
 
 mintemp.2006  <- raster("../data/01_01_2006.tiff")
 mintemp.2050 <- raster("../data/01_01_2050.tiff")
@@ -29,39 +29,4 @@ if (is.matrix(gsp) == FALSE){gsp <- data.matrix(gsp)}
 clust <- gClust(x=gsp, vp=mintemp.2006)
 out <- gENM(x=gsp, clust=clust, p=mintemp.2006) 
 gAnalysis(x=out)
-
-## Step 6. Making Histograms for each cluster
-
-
-gsp_bc <-  extract(mintemp.2006, gsp) 
-mt.c <- split(gsp_bc, clust)
-par(mfrow=c(1,3))
-#lapply(mt.c, hist)
-hrange <- c(265:280)
-
-pdf('~/Desktop/gENM/results/clust_hist.pdf', bg="black")
-par(mfrow=c(3,1))
-
-hist(mt.c$`1`,axes=F, col="mediumpurple1", xlim=c(265,280))
-axis(1, col = 'white', col.axis = 'white', col.ticks = 'white',cex.axis = 1.5, font = 1)
-axis(2, col = 'white', col.axis = 'white', col.ticks = 'white', cex.axis = 1.5, font =1)
-
-hist(mt.c$`2`, axes= F, col="yellow", xlim=c(265,280), ylim=c(0, 400))
-axis(1, col = 'white', col.axis = 'white', col.ticks = 'white',cex.axis = 1.5, font = 1)
-axis(2, col = 'white', col.axis = 'white', col.ticks = 'white', cex.axis = 1.5, font =1)
-
-hist(mt.c$`3`, col="turquoise1",axes= F,xlim=c(265,280))
-axis(1, col = 'white', col.axis = 'white', col.ticks = 'white',cex.axis = 1.5, font = 1)
-axis(2, col = 'white', col.axis = 'white', col.ticks = 'white', cex.axis = 1.5, font=1)
-
-
-hist(gsp_bc,col="violetred1", axes=F, xlim=c(265,280), ylim=c(0,400))
-
-axis(1, col = 'white', col.axis = 'white', col.ticks = 'white',cex.axis = 1.5, font = 1)
-axis(2, col = 'white', col.axis = 'white', col.ticks = 'white', cex.axis = 1.5, font=1)
-
-
-dev.off()
-system('open ../results/clust_hist.pdf')
-
 
