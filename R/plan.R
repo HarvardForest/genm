f20f143479842b2b14715bbf93d99137206fb5bf
unknown <annamcalderon@outlook.com>
2021-01-05 14:29:54 -0800

    overlays contour plot with observation points.

--- R/plan.R
+++ R/plan.R
@@ -15,21 +15,26 @@ plan <- drake_plan(
   Data[1826, c("Month", "Day", "Year")]<- c(05,01,1898),
   Data[1827, c("Month", "Day", "Year")]<- c(05,01,1898),
   Data[1828, c("Month", "Day", "Year")]<- c(07,14,1899),
-  
   Data <- transform(Data, Year = as.numeric(Year)),
   
   Data$Year_Bin <- cut(Data$Year, 28),
-  
-  sampling_distribution_yr = ggplot(Data, aes(x=longitude, y=latitude, color=factor(Year_Bin))) +
-    geom_point(shape=16, size=2) +
-    labs(title="Aphaenogaster Sampling Distribution by Year"), 
-  
-  data.loess <- loess(Year ~ longitude * latitude, data = Data),     
+  data.loess <- loess(Year ~ longitude * latitude, data = Data),       
   xgrid <-  seq(min(Data$longitude), max(Data$longitude), 0.5),
-  ygrid <-  seq(min(Data$latitude), max(Data$latitude), 0.5),
-  data.fit <-  expand.grid(longitude = xgrid, latitude = ygrid), 
-  mtrx3d <-  predict(data.loess, newdata = data.fit), 
-  contour(x = xgrid, y = ygrid, z = mtrx3d, xlab="longitude", ylab="latitude"), 
+  ygrid <-  seq(min(Data$latitude), max(Data$latitude), 0.5),  
+  data.fit <-  expand.grid(longitude = xgrid, latitude = ygrid),  
+  mtrx3d <-  predict(data.loess, newdata = data.fit),  
+  mtrx.melt <- melt(mtrx3d, id.vars = c('longitude' , 'latitude'), measure.vars =('Year_Bin')),
+  names(mtrx.melt) <- c('longitude', 'latitude', 'Year_Bin'),
+  mtrx.melt$longitude <- as.numeric(str_sub(mtrx.melt$longitude, str_locate(mtrx.melt$longitude, '=')[1,1] + 1)),  
+  mtrx.melt$latitude <- as.numeric(str_sub(mtrx.melt$latitude, str_locate(mtrx.melt$latitude, '=')[1,1] + 1)),
+  
+  sampling_distribution_year = ggplot(mtrx.melt, aes(x = longitude, y = latitude, z = Year_Bin)) +
+    stat_contour() + geom_point(data=Data, aes(color=Year_Bin)) + labs(title='Aphaenogaster ssp. Sampling Distribution by Year'),
+  
+  aph_hist <- ggplot(Data, aes(x=Year)) +
+    geom_histogram(binwidth = 28, fill="darkcyan", col="Black", size=1, alpha=0.65) + 
+    theme(panel.background = element_rect(fill="darkseagreen1")) +
+    labs(title = "Aphaenogaster Observations", y="Number of Observations"),
   
   BClim = brick("data/YbrevBC_2.5.grd")
   
